c662eb5f0885e06ab868326b855ad595976e99f4
Karla Agraz <karla.agraz@gmail.com>
2020-07-29 14:18:14 -0500

    iniciando la creación del efecto lightbox

---
+++ js/lightbox.js
@@ -0,0 +1,26 @@
+
+// Objeto con las propiedades del efecto Lightbox
+let propLightbox = {
+
+    imgContainer: document.getElementsByClassName('lightbox')
+    imagen: null
+}
+
+// Objeto con los métodos del efecto Lightbox
+let metLightbox = {
+    inicio: function() {
+
+        for (var i = 0; i < propLightbox.imgContainer.length; i++) {
+            propLightbox.imgContainer[i].addEventListener('click', metLightbox.capturaImagen)
+        }
+
+    },
+
+    capturaImagen: function() {
+
+        propLightbox.imagen = this;
+        metLightbox.lightbox(propLightbox.imagen);
+    }
+}
+
+metLightbox.inicio();
