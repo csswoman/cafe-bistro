dcb9d6fd084c9c854d064891aea4a1b2e8a7b694
Karla Agraz <karla.agraz@gmail.com>
2020-08-01 06:40:12 -0500

    efecto tabs finalizado

--- js/tabs.js
+++ js/tabs.js
@@ -1,35 +1,51 @@
+(function(){
 
+// Objeto con propiedades de Tab
+var propTabs = {
 
-//Objeto con propiedades de Tab
+	primer_encabezado: document.getElementById('encabezado_menu').firstElementChild,
+	primer_contenido: document.getElementById('contenido_menu').firstElementChild,
+	enlaces_encabezado: document.querySelectorAll('#encabezado_menu li a'),
+	li_encabezado: document.querySelectorAll('#encabezado_menu li'),
+	divs_contenido: document.querySelectorAll('#contenido_menu > div')
 
-let propTabs = {
-
-    primer_encabezado: document.getElementById('encabezado_menu').firstElementChild,
-    primer_contenido: document.getElementById('contenido_menu').firstElementChild,
-    enlaces_encabezado: document.querySelectorAll('#encabezado_menu li a'),
-    li_encabezado: document.querySelectorAll('#encabezado_menu li')
 }
 
+// Objeto con métodos de Tab
+var metTabs = {
+	
+	inicio: function(){
+		propTabs.primer_encabezado.className = 'active';
+		propTabs.primer_contenido.className = 'active';
 
-//Objeto con métodos de Tab
+		for (var i = 0; i < propTabs.enlaces_encabezado.length; i++) {
+			propTabs.enlaces_encabezado[i].addEventListener('click', metTabs.evento);
+		}
+	},
 
-let metTabs = {
+	evento: function(e){
+		e.preventDefault();
 
-    inicio: function(){
-        propTabs.primer_encabezado.className = 'active';
-        propTabs.primer_contenido.className = 'active';
+		for (var i = 0; i < propTabs.li_encabezado.length; i++) {
+			propTabs.li_encabezado[i].className = '';
+		}
 
-        for (let i = 0; i < propTabs.enlaces_encabezado.length; i++) {
-            propTabs.enlaces_encabezado[i].addEventListener('click', metTabs.evento);
-        }
-    },
+		for (var i = 0; i < propTabs.divs_contenido.length; i++) {
+			propTabs.divs_contenido[i].className = '';
+		}
 
-    evento: function(menu){
-        menu.preventDefault();
-        for (var i = 0; i < propTabs.li_encabezado.length; i++) {
-            propTabs.li_encabezado[i]
-        }
-    }
+		this.parentElement.className = 'active';
+        propTabs.contenido_activo = this.getAttribute('href');
+        document.querySelector(propTabs.contenido_activo).className = 'active';
+        document.querySelector(propTabs.contenido_activo).style.opacity = 0;
+        setTimeout(function(){
+            document.querySelector(propTabs.contenido_activo).style.opacity = 1;
+        }, 100)
+	}
+
 }
 
 metTabs.inicio();
+}())
+
+
