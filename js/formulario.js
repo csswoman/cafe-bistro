986e5c4ec46f20228c5f549eb4cdc6bf133e5dde
Karla Agraz <karla.agraz@gmail.com>
2020-08-01 17:00:59 -0500

    terminando efecto material design de formulario

--- js/formulario.js
+++ js/formulario.js
@@ -13,10 +13,22 @@ let metFormulario = {
     inicio: function() {
         for (var i =0; i < propFormulario.elementos.length; i++) {
 
-            if ( propFormulario.elementos[i].type == 'text' || propFormulario.elementos[i].type == 'email' || propFormulario.elementos[i].nodeName == 'textarea'){
+            if ( propFormulario.elementos[i].type == 'text' || propFormulario.elementos[i].type == 'email' || propFormulario.elementos[i].nodeName.toLowerCase() == 'textarea'){
                 propFormulario.elementos[i].addEventListener('focus', metFormulario.focusInput);
                 propFormulario.elementos[i].addEventListener('blur', metFormulario.blurInput);
             }
         }
+    },
+
+    focusInput: function () {
+        this.parentElement.children[1].className = 'label active';
+    },
+
+    blurInput: function () {
+        if(this.value == '') {
+            this.parentElement.children[1].className = 'label';
+        }
     }
 }
+
+metFormulario.inicio();
